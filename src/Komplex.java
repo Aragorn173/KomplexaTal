d6b7b82de89c84a750ddba015d236e821c4c0476
Aragorn173 <77720219+Aragorn173@users.noreply.github.com>
2022-03-29 15:10:43 +0200

    lade till: toSring, equals och lyckades att få alla delar att fungera och lösa uppgiften.

--- src/Komplex.java
+++ src/Komplex.java
@@ -1,8 +1,39 @@
+import java.util.Objects;
+
 public class Komplex {
     double a;
     double b;
 
     public Komplex(double v, double v1) {
+        this.a = v;
+        this.b = v1;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Komplex{" +
+                "a=" + a +
+                ", b=" + b +
+                '}';
+    }
+
+
+    public Komplex(double a) {
+        this.a = a;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Komplex komplex = (Komplex) o;
+        return Double.compare(komplex.a, a) == 0 && Double.compare(komplex.b, b) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b);
     }
 
 
@@ -11,16 +42,18 @@ public class Komplex {
         return c;
     }
 
+
+
     public Komplex multiply(Komplex b) {
 
-        return b;
+        Komplex c = new Komplex(a * b.a - this.b * b.b, this.b * b.a + a * b.b);
+
+        return c;
     }
+
 
     public double arg() {
-
-       return a;
+       double x = Math.atan(b/a);
+       return x;
     }
-
-
-
 }
